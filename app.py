3e5eee723b27b32722fcd98bec8a599fb9d4634a
alexanderchoske <alexandre.choski@gmail.com>
2025-08-21 22:22:56 -0300

    fix paginas de erros da aplicação

--- app.py
+++ app.py
@@ -229,9 +229,17 @@ print("[DEBUG] ============================\n")
 def unauthorized_error(error):
     return render_template('errors/401.html'), 401
 
+@app.errorhandler(403)
+def forbidden_error(error):
+    return render_template('errors/403.html'), 403
+
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template('errors/404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('errors/500.html'), 500
 
 @app.route('/')
 def index():
